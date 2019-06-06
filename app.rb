14e4d1adb5571f3c8bceaf1a5cf0f76ce4af825e
Meghan Miller <mille265@gmail.com>
2019-06-06 16:58:01 -0400

    stuff

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
+
+    get "/" do 
+        erb :user_input
+    end
+
+    post "/" do
+        sentence = params[:english]
+        binding.pry
+        pig = Piglatinizer.latinize(sentence)
+        redirect "/#{pig}"
+    end
+
+    get "/:words" do
+        @modified_sentence = params[:words]
+        erb :user_output
+    end
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,20 @@
+class Piglatinizer
+
+    def self.latinize(string)
+        string = string.downcase.split(” “)
+        arr = string.collect do |word|
+        a = /[aeiou]/ =~ word
+            if a == 0
+                word = word + “way”
+            elsif a == 1
+                word = word[1..-1] + word[0] + “ay”
+            elsif a == 2
+                word = word[2..-1] + word[0..1] + “ay”
+            elsif a == 3
+                word = word[3..-1] + word[0..2] + “ay”
+            else
+                word
+            end
+        end
+    end 
+end 
