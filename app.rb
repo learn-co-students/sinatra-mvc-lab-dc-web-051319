20ee9db322110920cb1c1a9821be6a10d22f5c14
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-07 03:16:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
+set :views, "views" 
+
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :user_input
+  end 
+  
+  post '/piglatinize' do
+  user_phrase = params[:user_phrase]
+  piglatinizer = PigLatinizer.new 
+  @result = piglatinizer.piglatinize(user_phrase)
+    erb :show
+  end 
 end
